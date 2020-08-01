df98b3d66bc0471353d06c4d280b6d25a3a2e51a
lukas-durica <renegadefor7>
2020-08-01 20:45:37 +0200

    minor changes

--- src/benchmarking/MinBinaryHeapTest.gd
+++ src/benchmarking/MinBinaryHeapTest.gd
@@ -16,24 +16,29 @@ func do_benchmark(number_of_elements):
 	var new_heap = MinBinaryHeap.new()
 	var time_start = OS.get_ticks_usec()
 	for i in rand_array:
-		new_heap.insert_key({val = i, cell = null})
+		new_heap.insert_key({value = i, cell = null})
 	var elapsed = OS.get_ticks_usec() - time_start
 	print()
 	print("Number of elements: ", number_of_elements)
 	print()
 	print("===MinBinaryHeap===")
 	print("Inserting ", number_of_elements, 
-			" nodes, elapsed time: ", elapsed)
+			" nodes, elapsed time: ", elapsed, " microseconds")
 	print("Inserting ", number_of_elements, 
-			" nodes, average time: ", elapsed / float(number_of_elements))
+			" nodes, average time: ", elapsed / float(number_of_elements), 
+			" microseconds")
 	
 	time_start = OS.get_ticks_usec()
 	while not new_heap.empty():
 		new_heap.extractMin()
 	elapsed = OS.get_ticks_usec() - time_start
 	print("Extracting ", number_of_elements, 
-			" nodes, elapsed time: ", elapsed)
+			" nodes, elapsed time: ", elapsed, " microseconds")
 	print("Extracting ", number_of_elements, 
-			" nodes, average time: ", elapsed / float(number_of_elements))
-	print()	
+			" nodes, average time: ", elapsed / float(number_of_elements), 
+			" microseconds")
+	print()
+	
+
+	
 	
