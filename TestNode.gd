fe63fa02898627a52f4a8e5eaa00d23c51aee9dc
lukas-durica <48993236+lukas-durica@users.noreply.github.com>
2021-02-17 22:41:51 +0100

    add function for get first free interval

--- TestNode.gd
+++ TestNode.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
 
-var test_arr = [Vector2(2, 3), Vector2(4, 5), Vector2(8, 9)]
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#print(test_arr.bsearch(Vector2(4,5)))
-	print(get_firs_free_interval(Vector2(3, 4)))
 	
-func get_firs_free_interval(interval : Vector2):
+	#get_first_free_interval(Vector2(3,4))
+	test_intervals()
+func get_first_free_interval(interval : Vector2):
 	#if not vertex in intervals:
 	#	return interval
 	
@@ -16,28 +14,27 @@ func get_firs_free_interval(interval : Vector2):
 	#if test_arr.empty():
 	#	push_error("vertex intervals are empty!")
 	#	return
-	var idx = test_arr.bsearch(interval, false)
-	print(idx)
-	if idx == 0:
-		if interval.y < test_arr[idx].x:
-			return interval 
+	
+	
+	
+	
+	#print("idx: ", idx)
+	# test without this
+	
+	# tracking if the idx updated
+	# if not interval can be returned, else the closest interval
+	# can be returned
+	var idx = intervals.bsearch_custom(interval, self, "test_search", false)
 	var size = interval.y - interval.x
-	if idx == test_arr.size():
-		if test_arr[idx - 1].y < interval.x:
+	var prev_value = intervals[idx - 1].y if idx > 0 else -1
+	var next_value = intervals[idx].x if idx < intervals.size() else INF
+	if prev_value < interval.x and interval.y < next_value:
 			return interval
+	while idx <= intervals.size():
+		if next_value - prev_value > size:
+			return Vector2(prev_value + 1, prev_value + 1 + size)
+		idx+=1
+		prev_value = intervals[idx - 1].y if idx > 0 else -1
+		next_value = intervals[idx].x if idx < intervals.size() else INF
 		
-		return Vector2(test_arr[idx-1].y + 1, test_arr[idx-1].y + size + 1)
-	
-	if interval.x > test_arr[idx - 1].y and interval.y < test_arr[idx].x:
-		return interval
-	
-	while idx != test_arr.size() -1:
-		if test_arr[idx].x - test_arr[idx - 1].y > size:
-			return Vector2(test_arr[idx-1].y + 1, test_arr[idx-1].y + size + 1)
-		idx += 1
-	return Vector2(test_arr[test_arr.size() - 1].y + 1, 
-			test_arr[test_arr.size() - 1].y + size + 1)
-	
-	# osetrit zaciatok
-	
-		
+
--- src/algorithms/AStarSIPP.gd
+++ src/algorithms/AStarSIPP.gd
@@ -1,6 +1,6 @@
 class_name AStarSIPP extends GridBasedAlgorithm
 
-var intervals : = {}
+var vertex_intervals : = {}
 
 func find_solution(starts_and_goals : Array):
 # The key idea for all of these algorithms is that we keep track of an 
@@ -86,19 +86,47 @@ func reconstruct_path(goal : Vector2, came_from : Dictionary) -> Array:
 	path.invert()
 	return path
 
-func get_firs_free_interval(vertex : Vector2, interval_start : int, size : int):
-	if not vertex in intervals:
-		return Vector2(interval_start, interval_start + size)
+func get_first_free_interval(intervals : Array, interval : Vector2):
+	var idx = intervals.bsearch_custom(interval, self, "binary_search", false)
+	var size = interval.y - interval.x
+	var prev_value = intervals[idx - 1].y if idx > 0 else -1
+	var next_value = intervals[idx].x if idx < intervals.size() else INF
+	if prev_value < interval.x and interval.y < next_value:
+			return interval
+	while idx <= intervals.size():
+		if next_value - prev_value > size:
+			return Vector2(prev_value + 1, prev_value + 1 + size)
+		idx+=1
+		prev_value = intervals[idx - 1].y if idx > 0 else -1
+		next_value = intervals[idx].x if idx < intervals.size() else INF
 	
-	var vertex_intervals = intervals[vertex]
-	if vertex_intervals.empty():
-		push_error("vertex intervals are empty!")
-		return
-	var idx = vertex_intervals.bsearch(Vector2(interval_start, 
-			interval_start + size))
-	if idx == vertex_intervals.size():
-		return Vector2(vertex_intervals[idx].y + 1, vertex_intervals[idx].y + 1)
-	else:
-		pass
-	
+func binary_search(a : Vector2, b : Vector2):
+	return a.y < b.x
 
+func test_intervals():
+	var test_dict = {Vector2(0, 1) : Vector2(0, 1), 
+					Vector2(1, 2) : Vector2(6, 7),
+					Vector2(2, 3) : Vector2(6, 7), 
+					Vector2(3, 4) : Vector2(6, 7),
+					Vector2(4, 5) : Vector2(6, 7),
+					Vector2(5, 6) : Vector2(6, 7),
+					Vector2(6, 7) : Vector2(6, 7),
+					Vector2(7, 8) : Vector2(13, 14),
+					Vector2(9, 10) : Vector2(13, 14),
+					Vector2(12, 16) : Vector2(19, 23),
+					Vector2(16, 17) : Vector2(19, 20),
+					Vector2(17, 18) : Vector2(19, 20),
+					Vector2(18, 19) : Vector2(19, 20),
+					Vector2(21, 22) : Vector2(21, 22)} 
+
+	var intervals = [Vector2(2, 3), Vector2(4, 5), Vector2(8, 9), 
+			Vector2(9, 10), Vector2(11, 12), Vector2(16, 18)]
+	
+	for test_value in test_dict:
+		var test = get_first_free_interval(test_value, intervals)
+		if test == test_dict[test_value]:
+			print("OK Interval: {0}: {1} ".format([test_value, 
+					test_dict[test_value]]))
+		else:
+			print("Error Interval: {0}: {1} is {2} ".format([test_value, 
+					test_dict[test_value], test]))
